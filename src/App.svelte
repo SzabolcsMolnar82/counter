0e5d61f061fea35365d338d80665784f5ce6ff31
Szabolcs Molnár <szabolcsmolnar82@gmail.com>
2024-11-01 07:56:20 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  let count = $state(0)
+</script>
+
+<div class="felx-gap">
+  <input type="number" bind:value={count} disabled>
+  <button on:click={() => count ++}>count is {count}</button>
+</div>
+
+
+<style>
+  div {
+    background-color: #5B676D;
+    border-radius: 30px
+  }
+  .felx-gap {
+    display: flex;
+    gap: 10px;
+  }
+  button {
+    padding: 10px;
+    text-align: center;
+    font-family: 'Times New Roman', serif;
+    font-size:60px
+  }
+  input {
+    padding: 2px;
+    text-align: center;
+    font-family: 'Times New Roman', serif;
+    font-size:50px
+    
+  }
+</style>
